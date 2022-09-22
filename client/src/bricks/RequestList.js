ea18615f6b9e8ae9d0c027e57662001d7246c206
Pavel Nový <pavel.novy@unicorn.com>
2022-09-22 17:04:44 +0200

    Request list + nový server

--- client/src/bricks/RequestList.js
+++ client/src/bricks/RequestList.js
@@ -1,5 +1,4 @@
 import {useEffect, useState} from "react";
-
 import Icon from "@mdi/react";
 import { mdiLoading } from "@mdi/js";
 import Request from './Request'
@@ -7,15 +6,14 @@ import Request from './Request'
 function RequestList(){
 
     const [requestListLoadCall, setRequestListLoadCall] = useState({
-        state: "pending",
+        state: "pending"
     });
 
     useEffect(() => {
-
         fetch(`http://localhost:3000/request/list`, {
             method: "GET",
             headers:{
-                'Authorization': 'Bearer '+'qdsMkMpb16'
+                Authorization: 'Bearer '+'qdsMkMpb16'
             }
         }).then(async (response) => {
             const responseJson = await response.json();
@@ -27,11 +25,13 @@ function RequestList(){
         });
     }, []);
 
+    console.log(requestListLoadCall.data)
+
     function getChild() {
         switch (requestListLoadCall.state) {
             case "pending":
                 return (
-                    <div className={""}>
+                    <div>
                         <Icon size={2} path={mdiLoading} spin={true} />
                     </div>
                 );
@@ -43,7 +43,7 @@ function RequestList(){
                 );
             case "error":
                 return (
-                    <div className={""}>
+                    <div>
                         <div>Nepodařilo se načíst data o žádostech.</div>
                         <br />
                         <pre>{JSON.stringify(requestListLoadCall.error, null, 2)}</pre>
